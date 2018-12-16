768cf1bbe1ec88ceec260ce55bf42fbbf130474c
nodarsan <snodar@10pines.com>
2018-12-15 21:01:11 -0300

    Agregar archivos que faltaban

---
+++ Assets/ItemNetworkManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ItemNetworkManager : NetworkBehaviour
+{
+	public GameObject itemPrefab;
+	public int numberOfItems;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public override void OnStartServer()
+	{
+		for (int i=0; i < numberOfItems; i++)
+		{
+			Debug.Log ("Created coso");
+			var spawnPosition = new Vector3(
+				Random.Range(-2.0f, 2.0f),
+				Random.Range(-2.0f, 2.0f),
+				0.0f);
+
+			var spawnRotation = Quaternion.Euler( 
+				0.0f, 
+				0.0f, 
+				0.0f);
+
+			var item = (GameObject)Instantiate(itemPrefab, spawnPosition, spawnRotation);
+			NetworkServer.Spawn(item);
+		}
+	}
+}
